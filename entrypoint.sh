6de5188058dea6f22c77268040a66fb13e5ceaed
simbam1 <simbam1@umbc.edu>
2021-07-01 12:48:10 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,7 +22,7 @@ if [ ${INPUT_REMOTE_HOST#"ssh://"} != "$INPUT_REMOTE_HOST" ]; then
     # Save private key to a file and register it with the agent.
     mkdir -p "$HOME/.ssh"
     printf '%s' "$INPUT_SSH_PRIVATE_KEY" > "$HOME/.ssh/docker"
-    chmod 600 "$HOME/.ssh/docker"
+    chmod -R 600 "$HOME/.ssh"
     eval $(ssh-agent)
     ssh-add "$HOME/.ssh/docker"
 
