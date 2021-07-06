18789a8a840561f8f939bf74397650da6d312868
simbam1 <simbam1@umbc.edu>
2021-07-05 20:01:08 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,21 +20,15 @@ if [ ${INPUT_REMOTE_HOST#"ssh://"} != "$INPUT_REMOTE_HOST" ]; then
     echo "Registering SSH keys..."
 
     # Save private key to a file and register it with the agent.
-    ls -l "$HOME/.ssh"
+
     mkdir -p "$HOME/.ssh"
-    ls -l "$HOME/.ssh"
     printf '%s' "$INPUT_SSH_PRIVATE_KEY" > "$HOME/.ssh/docker"
-    ls -l "$HOME/.ssh"
     chmod -R 600 "$HOME/.ssh"
-    cp /tmp/config "$HOME/.ssh/config"
+    #cp /tmp/config "$HOME/.ssh/config"
     ssh-keyscan -H "$SSH_HOST" > "$HOME/.ssh/known_hosts"
-    chmod 400 "$HOME/.ssh/config"
-    cat "$HOME/.ssh/config"
-    ls -l "$HOME/.ssh"
+    #chmod 400 "$HOME/.ssh/config"
     eval $(ssh-agent)
-    ls -l "$HOME/.ssh"
     ssh-add "$HOME/.ssh/docker"
-    ls -l "$HOME/.ssh"
     whoami
 
 
